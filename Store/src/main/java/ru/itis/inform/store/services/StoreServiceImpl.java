278ba1c30e761ed757017d6b1d08bdf6bdf8a710
Arthur <galiullinko@gmail.com>
2016-02-16 10:11:56 +0300

    Add logging

--- Store/src/main/java/ru/itis/inform/store/services/StoreServiceImpl.java
+++ Store/src/main/java/ru/itis/inform/store/services/StoreServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.itis.inform.store.services;
 
 
+import org.apache.log4j.Logger;
 import ru.itis.inform.store.dao.ItemsDao;
 import ru.itis.inform.store.dao.ItemsDaoFileBasedImpl;
 import ru.itis.inform.store.dao.models.Item;
@@ -10,16 +11,20 @@ import java.io.IOException;
 public class StoreServiceImpl implements StoreService {
 
     ItemsDao itemsDao;
+    private static final Logger log = Logger.getLogger(StoreServiceImpl.class);
 
     public StoreServiceImpl(ItemsDao itemsDao) {
         this.itemsDao = itemsDao;
+        log.info("Constructor StoreServiceImpl");
     }
 
     public void buy(String itemName) {
         itemsDao.delete(itemName);
+        log.info("Buy StoreServiceImpl");
     }
 
     public boolean isExist(String itemName) {
+        log.info("isExist StoreServiceImpl");
         return itemsDao.select(itemName) != null;
     }
 }
